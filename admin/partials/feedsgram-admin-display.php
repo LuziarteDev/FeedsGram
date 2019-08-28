16c7657400e9a9618ad04314fab592aa48fea914
LuziarteDev <joseyesfox@gmail.com>
2019-08-28 19:20:25 -0300

    Second Commit

--- admin/partials/feedsgram-admin-display.php
+++ admin/partials/feedsgram-admin-display.php
@@ -12,11 +12,34 @@
  * @subpackage Feedsgram/admin/partials
  */
 ?>
-
-			<div class="welcome-panel-column">
-				<h3><span class="dashicons dashicons-shield" aria-hidden="true"></span> Problemas com spam? Você tem proteção.</h3>
-				<p>Para robôs de spam tudo é um alvo; seus formulários de contato não são exceção. Antes de ser atingido por eles, proteja seus formulários de contato com a poderosa ferramenta anti-spam que o Contact Form 7 fornece.</p>
-				<p>Contact Form 7 suporta filtragem de spam com <a href="https://contactform7.com/spam-filtering-with-akismet/">Akismet</a>. Bloqueios <a href="https://contactform7.com/recaptcha/">reCAPTCHA</a> inteligentes de "spambots" irritantes. Além disso, usando <a href="https://contactform7.com/comment-blacklist/">lista negra de comentários</a>, você pode bloquear mensagens contendo palavras-chave específicas ou aquelas enviadas de endereços IP específicos.</p>
+<div class="fg-main">
+	<h1 class="fg-title-feedsgram">FeedsGram</h1>
+	<div class="fg-welcome">
+		<p>Bem vindo ao FeedsGram, plugin responsável por renderizar posts do Instagram sem necessidade do token, permitindo seu site carregar fotos dos posts de perfis públicos do Instagram</p>
+		<p>Para mostrar posts do Instagram utilize o shortcode:</p>
+		<p><span>[feedsgram_posts]</span></p>
+	</div>
+	<form class="fg-form-post" action="">
+		<div class="fg-blocks">
+			<div class="fg-block">
+				<h4>Configurações: </h3>
+				<label>Url do Perfil</label>
+				<input type="text" name="fg_profile" placeholder="https://www.instagram.com/profile/" required>
+				
+				<label>Quantidade de posts</label>
+				<input type="number" name="fg_number_pics" placeholder="1" required>
 			</div>
-<h1>Instagram Feed</h1>
+			<div class="fg-block">
+				<h4>Layout Desejado( <i>Em breve</i> ):</h3>
+				<label>Escolha o layout desejado</label>
+				<select type="text" name="fg_layout" value="Layout" disabled>
+					<option value="Layout">Selecione layout desejado</option>
+					<p><i>Pré-visualização:</i></p>
+				</select>
+			</div>
+		</div>
+		<button type="submit">Salvar</button>
+	</form>
+
+</div>
 
